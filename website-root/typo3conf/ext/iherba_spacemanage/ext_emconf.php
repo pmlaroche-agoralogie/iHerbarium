07cc6b6780985040f868645d75c53e918b1157f6
pmlaroche-agoralogie <philippe.laroche@agoralogie.fr>
2012-10-01 10:28:56 +0200

    add functionnality

---
+++ website-root/typo3conf/ext/iherba_spacemanage/ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "iherba_spacemanage".
+#
+# Auto generated 01-10-2012 10:28
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => '',
+	'description' => '',
+	'category' => '',
+	'author' => '',
+	'author_email' => '',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => '',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"c436";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"7fed";s:14:"ext_tables.php";s:4:"ba51";s:16:"locallang_db.xml";s:4:"9746";s:19:"doc/wizard_form.dat";s:4:"0449";s:20:"doc/wizard_form.html";s:4:"73c2";s:38:"pi1/class.tx_iherbaspacemanage_pi1.php";s:4:"4f6c";s:17:"pi1/locallang.xml";s:4:"684b";s:38:"pi2/class.tx_iherbaspacemanage_pi2.php";s:4:"659e";s:17:"pi2/locallang.xml";s:4:"c0d9";s:38:"pi3/class.tx_iherbaspacemanage_pi3.php";s:4:"4bef";s:17:"pi3/locallang.xml";s:4:"5a0c";s:38:"pi4/class.tx_iherbaspacemanage_pi4.php";s:4:"609f";s:17:"pi4/locallang.xml";s:4:"7958";}',
+);
+
+?>
---
+++ website-root/typo3conf/ext/iherba_spacemanage/pi1/class.tx_iherbaspacemanage_pi1.php
@@ -0,0 +1,82 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2012  <>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * [CLASS/FUNCTION INDEX of SCRIPT]
+ *
+ * Hint: use extdeveval to insert/update function index above.
+ */
+
+require_once(PATH_tslib.'class.tslib_pibase.php');
+
+
+/**
+ * Plugin 'list_space' for the 'iherba_spacemanage' extension.
+ *
+ * @author	 <>
+ * @package	TYPO3
+ * @subpackage	tx_iherbaspacemanage
+ */
+class tx_iherbaspacemanage_pi1 extends tslib_pibase {
+	var $prefixId      = 'tx_iherbaspacemanage_pi1';		// Same as class name
+	var $scriptRelPath = 'pi1/class.tx_iherbaspacemanage_pi1.php';	// Path to this script relative to the extension dir.
+	var $extKey        = 'iherba_spacemanage';	// The extension key.
+	
+	/**
+	 * The main method of the PlugIn
+	 *
+	 * @param	string		$content: The PlugIn content
+	 * @param	array		$conf: The PlugIn configuration
+	 * @return	The content that is displayed on the website
+	 */
+	function main($content, $conf) {
+		$this->conf = $conf;
+		$this->pi_setPiVarDefaults();
+		$this->pi_loadLL();
+		$this->pi_USER_INT_obj = 1;	// Configuring so caching is not expected. This value means that no cHash params are ever set. We do this, because it's a USER_INT object!
+	
+		$content='
+			<strong>This is a few paragraphs:</strong><br />
+			<p>This is line 1</p>
+			<p>This is line 2</p>
+	
+			<h3>This is a form:</h3>
+			<form action="'.$this->pi_getPageLink($GLOBALS['TSFE']->id).'" method="POST">
+				<input type="text" name="'.$this->prefixId.'[input_field]" value="'.htmlspecialchars($this->piVars['input_field']).'">
+				<input type="submit" name="'.$this->prefixId.'[submit_button]" value="'.htmlspecialchars($this->pi_getLL('submit_button_label')).'">
+			</form>
+			<br />
+			<p>You can click here to '.$this->pi_linkToPage('get to this page again',$GLOBALS['TSFE']->id).'</p>
+		';
+	
+		return $this->pi_wrapInBaseClass($content);
+	}
+}
+
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/iherba_spacemanage/pi1/class.tx_iherbaspacemanage_pi1.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/iherba_spacemanage/pi1/class.tx_iherbaspacemanage_pi1.php']);
+}
+
+?>
