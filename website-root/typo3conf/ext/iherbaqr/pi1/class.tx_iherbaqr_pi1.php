06de19b188815b695935c738abb6ee363ddbc7fd
pmlaroche-agoralogie <philippe.laroche@agoralogie.fr>
2012-10-20 16:34:26 +0200

    better iherbarium page for paper clipping

--- website-root/typo3conf/ext/iherbaqr/pi1/class.tx_iherbaqr_pi1.php
+++ website-root/typo3conf/ext/iherbaqr/pi1/class.tx_iherbaqr_pi1.php
@@ -55,28 +55,51 @@ class tx_iherbaqr_pi1 extends tslib_pibase {
 		$this->pi_setPiVarDefaults();
 		$this->pi_loadLL();
 		$this->pi_USER_INT_obj = 1;	// Configuring so caching is not expected. This value means that no cHash params are ever set. We do this, because it's a USER_INT object!
-	
-		$urlqrecnode = "http://www.iherbarium.org/index.php?id=21&numero_observation=";
-		$urlqrecnode .= desamorcer($_GET['numero_observation']);
-		$urlgoogle =  urlencode($urlqrecnode);
+		
+		$currentobs = desamorcer($_GET['numero_observation']);
+		$urlqrencode = "http://www.iherbarium.org/observation/data/numero_observation=".$currentobs;
+		$urlgoogle =  urlencode($urlqrencode);
 		$content='<table style="text-align: left; width: 100%;" border="0" cellpadding="2" cellspacing="2"> <tbody> <tr> <td style="width: 100px;">
-			<img src=http://chart.apis.google.com/chart?chs=350x350&cht=qr&chld=H&chl='.$urlgoogle.' width=100><br>	
+			<img src=http://chart.apis.google.com/chart?chs=380x380&cht=qr&chld=H&chl='.$urlgoogle.' width=100><br>	
 			</td> <td> 
 		';
 	/*Affichage des informations concernant l'observation */
 		bd_connect();
-		$sql="select date_depot,idobs,commentaires,longitude,latitude from iherba_observations where idobs=".desamorcer($_GET['numero_observation'])." ";
+		$sql="select date_depot,idobs,commentaires,longitude,latitude from iherba_observations where idobs=".$currentobs." ";
 	    $result = mysql_query($sql)or die ();
 	    if(!($lobervation = mysql_fetch_assoc($result))) return ; 
 		$date_depot = $lobervation["date_depot"];
 		$dateaffichage = str_replace("-","/",$date_depot) ;
-		$content.= "Observation : "." ".desamorcer($_GET['numero_observation'])."<br/>\n";//$this->pi_getLL('numeroObservation', '', 1)
-		$content.= "Date  : "." ".$dateaffichage."<br/>\n";//$this->pi_getLL('numeroObservation', '', 1)
+		$content.= $this->pi_getLL('text_before_obs_num', '', 1)." ".$currentobs."<br/>\n";
+		$content.= $this->pi_getLL('text_before_date', '', 1)." ".$dateaffichage."<br/>\n";
+		$content.= $this->pi_getLL('text_before_localisation', '', 1)." ".$lobervation["latitude"]." , ".$lobervation["longitude"]."<br/>\n";
 		if($lobervation["commentaires"] !=""){
-			$content.="Commentaire : "." ".$lobervation["commentaires"]."<br/>\n";//$this->pi_getLL('commentairesObservation', '', 1)
+			$content.=$this->pi_getLL('text_before_comment', '', 1)." ".$lobervation["commentaires"]."<br/>\n";
 		}
+
+		//display last given name
+		$champscomment = 'email_comment';
+		$sql_determination.="select iherba_determination.id , nom_commun,nom_scientifique,date, famille,genre ,id_cases, iherba_determination_cases.$champscomment ,iherba_certitude_level.value as certitude_level, iherba_certitude_level.comment as certitude_comment,";
+		$sql_determination.=" iherba_determination.comment,iherba_precision_level.value as precision_level,iherba_precision_level.$champscomment as precisioncomment from iherba_determination,iherba_determination_cases,iherba_certitude_level, iherba_precision_level ";
+		$sql_determination.="where  iherba_determination_cases.language = 'fr' and iherba_determination_cases.id_cases = iherba_determination.comment_case AND iherba_determination.precision_level = iherba_precision_level.value ";
+		$sql_determination.= " AND iherba_determination.certitude_level = iherba_certitude_level.value AND iherba_determination.id_obs=$currentobs ";
+		$sql_determination.= " order by creation_timestamp desc";
+		$sql_determination .= " limit 1";
 		
-		$sql="select nom_photo_final from iherba_photos where id_obs=".desamorcer($_GET['numero_observation']);
+		 // echo "<!-- sql $sql_determination -->";
+		
+		$result_determination = mysql_query($sql_determination) or die ($sql_determination);
+		$num_rows = mysql_num_rows($result_determination); //nombre de lignes rÈsultats
+			
+		if ($row_determination = mysql_fetch_assoc($result_determination)) {
+		    $nom_commun=$row_determination["nom_commun"];
+		    $nom_scientifique=$row_determination["nom_scientifique"];
+		    $date=$row_determination["date"];
+		    $content.= $this->pi_getLL('text_before_determination', '', 1) .$date.") : ".$nom_commun. " ".$nom_scientifique."<br>";
+		  }
+  
+		//display pictures
+		$sql="select nom_photo_final from iherba_photos where id_obs=".$currentobs;
 		$result = mysql_query($sql)or die ();
 		/* On affiche les photos que l'utilisateur nous a déjà fait parvenir */
 		$repertoire = "/medias/big/";
@@ -85,7 +108,8 @@ class tx_iherbaqr_pi1 extends tslib_pibase {
 			$content.='<img src="'.$image.'" border=0 height="65"  /></blank>&nbsp;';
 			}
 	
-		$content.= "</td> </tr> </tbody> </table> ";	
+	
+		
 		return $this->pi_wrapInBaseClass($content);
 	}
 }
