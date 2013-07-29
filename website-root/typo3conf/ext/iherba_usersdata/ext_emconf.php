5df97e164cb82424a928bec5e83e0e9327c8405b
pmlaroche-agoralogie <philippe.laroche@agoralogie.fr>
2013-07-29 10:21:35 +0200

    new plug in to manage user pref

---
+++ website-root/typo3conf/ext/iherba_usersdata/ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "iherba_usersdata".
+#
+# Auto generated 28-07-2013 17:44
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => '',
+	'description' => '',
+	'category' => '',
+	'author' => '',
+	'author_email' => '',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => '',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"ab41";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"11bf";s:14:"ext_tables.php";s:4:"1cd4";s:16:"locallang_db.xml";s:4:"0a03";s:19:"doc/wizard_form.dat";s:4:"712e";s:20:"doc/wizard_form.html";s:4:"6a3c";s:36:"pi1/class.tx_iherbausersdata_pi1.php";s:4:"a5cd";s:17:"pi1/locallang.xml";s:4:"2231";s:36:"pi2/class.tx_iherbausersdata_pi2.php";s:4:"eb7e";s:17:"pi2/locallang.xml";s:4:"b1aa";s:36:"pi3/class.tx_iherbausersdata_pi3.php";s:4:"9078";s:17:"pi3/locallang.xml";s:4:"3c1e";s:36:"pi4/class.tx_iherbausersdata_pi4.php";s:4:"7fe0";s:17:"pi4/locallang.xml";s:4:"b22b";}',
+);
+
+?>
