842fff43901fe6f70682e4d54bb4a7828ed20363
pmlaroche-agoralogie <philippe.laroche@agoralogie.fr>
2013-10-08 15:07:32 +0200

    show details about area

---
+++ website-root/typo3conf/ext/iherba_area_details/ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "iherba_area_details".
+#
+# Auto generated 08-10-2013 15:04
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => '',
+	'description' => '',
+	'category' => '',
+	'author' => '',
+	'author_email' => '',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => '',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"d9d9";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"94fa";s:14:"ext_tables.php";s:4:"a574";s:16:"locallang_db.xml";s:4:"bd95";s:19:"doc/wizard_form.dat";s:4:"563f";s:20:"doc/wizard_form.html";s:4:"df26";s:38:"pi1/class.tx_iherbaareadetails_pi1.php";s:4:"8e55";s:17:"pi1/locallang.xml";s:4:"7a2c";s:38:"pi2/class.tx_iherbaareadetails_pi2.php";s:4:"4a82";s:17:"pi2/locallang.xml";s:4:"071f";s:38:"pi3/class.tx_iherbaareadetails_pi3.php";s:4:"2ecd";s:17:"pi3/locallang.xml";s:4:"d9e9";s:38:"pi4/class.tx_iherbaareadetails_pi4.php";s:4:"2607";s:17:"pi4/locallang.xml";s:4:"c50b";s:38:"pi5/class.tx_iherbaareadetails_pi5.php";s:4:"1643";s:17:"pi5/locallang.xml";s:4:"0dae";s:38:"pi6/class.tx_iherbaareadetails_pi6.php";s:4:"13d6";s:17:"pi6/locallang.xml";s:4:"1540";}',
+);
+
+?>
---
+++ website-root/typo3conf/ext/iherba_area_details/ext_tables.php
@@ -0,0 +1,70 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+t3lib_div::loadTCA('tt_content');
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key,pages';
+
+
+t3lib_extMgm::addPlugin(array(
+	'LLL:EXT:iherba_area_details/locallang_db.xml:tt_content.list_type_pi1',
+	$_EXTKEY . '_pi1',
+	t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif'
+),'list_type');
+
+
+t3lib_div::loadTCA('tt_content');
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi2']='layout,select_key,pages';
+
+
+t3lib_extMgm::addPlugin(array(
+	'LLL:EXT:iherba_area_details/locallang_db.xml:tt_content.list_type_pi2',
+	$_EXTKEY . '_pi2',
+	t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif'
+),'list_type');
+
+
+t3lib_div::loadTCA('tt_content');
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi3']='layout,select_key,pages';
+
+
+t3lib_extMgm::addPlugin(array(
+	'LLL:EXT:iherba_area_details/locallang_db.xml:tt_content.list_type_pi3',
+	$_EXTKEY . '_pi3',
+	t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif'
+),'list_type');
+
+
+t3lib_div::loadTCA('tt_content');
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi4']='layout,select_key,pages';
+
+
+t3lib_extMgm::addPlugin(array(
+	'LLL:EXT:iherba_area_details/locallang_db.xml:tt_content.list_type_pi4',
+	$_EXTKEY . '_pi4',
+	t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif'
+),'list_type');
+
+
+t3lib_div::loadTCA('tt_content');
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi5']='layout,select_key,pages';
+
+
+t3lib_extMgm::addPlugin(array(
+	'LLL:EXT:iherba_area_details/locallang_db.xml:tt_content.list_type_pi5',
+	$_EXTKEY . '_pi5',
+	t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif'
+),'list_type');
+
+
+t3lib_div::loadTCA('tt_content');
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi6']='layout,select_key,pages';
+
+
+t3lib_extMgm::addPlugin(array(
+	'LLL:EXT:iherba_area_details/locallang_db.xml:tt_content.list_type_pi6',
+	$_EXTKEY . '_pi6',
+	t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif'
+),'list_type');
+?>
